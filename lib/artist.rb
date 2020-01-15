573ad3c44e36b204a213e32ffeba07a2f04628b1
myk245 <myk245@nyu.edu>
2020-01-15 14:15:07 -0500

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,28 +4,36 @@ class Artist
   attr_accessor :name
   attr_reader :songs
 
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable::InstanceMethods 
+
   @@artists = []
 
   def initialize
-    @@artists << self
+    super
     @songs = []
   end
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
+  # we have defined the find_by_name method in the Findable module, so we can extend it and use it in this class 
+  # def self.find_by_name(name)
+  #   @@artists.detect{|a| a.name == name}
+  # end
 
   def self.all
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
+  # by extending the Memorable module, we have access to the two below methods, 
+  # so we do not need to write these out again 
+  # def self.reset_all
+  #   self.all.clear
+  # end
 
-  def self.count
-    self.all.count
-  end
+  # def self.count
+  #   self.all.count
+  # end
 
   def add_song(song)
     @songs << song
@@ -36,7 +44,9 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # defined in Paramble 
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
+
 end
--- lib/song.rb
+++ lib/song.rb
@@ -4,33 +4,43 @@ class Song
   attr_accessor :name
   attr_reader :artist
 
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable::InstanceMethods 
+
   @@songs = []
 
-  def initialize
-    @@songs << self
-  end
+  # def initialize
+  #   @@songs << self
+  # end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+  # we have defined the find_by_name method in the Findable module, so we can extend it and use it in this class 
+  # def self.find_by_name(name)
+  #   @@songs.detect{|a| a.name == name}
+  # end
 
   def self.all
     @@songs
   end
 
-  def self.reset_all
-    self.all.clear
-  end
+  # by extending the Memorable module, we have access to the two below methods, 
+  # so we do not need to write these out again 
+  # def self.reset_all
+  #   self.all.clear
+  # end
 
-  def self.count
-    self.all.count
-  end
+  # def self.count
+  #   self.all.count
+  # end
 
   def artist=(artist)
     @artist = artist
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # defined in Paramble module 
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
+
 end
